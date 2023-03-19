f42a438786122d45a3d39bd2bc9d94b3ef3ce073
Michael Sarkisian <msarkisian1996@gmail.com>
2023-03-19 15:04:46 -0400

    aws sqs enqueue

--- broker/main.go
+++ broker/main.go
@@ -3,13 +3,37 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
 )
 
-func HandleRequest(ctx context.Context, event interface{}) (string, error) {
-	fmt.Printf("%v\n", event)
-	return "Hello from Lambda!", nil
+type MittenEvent struct {
+	Word string
+}
+
+func HandleRequest(ctx context.Context, event MittenEvent) (string, error) {
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		return "", fmt.Errorf("unable to load SDK config, %v", err)
+	}
+
+	client := sqs.NewFromConfig(cfg)
+	queueUrl := os.Getenv("QUEUE_URL")
+
+	input := &sqs.SendMessageInput{
+		MessageBody: aws.String(event.Word),
+		QueueUrl:    aws.String(queueUrl),
+	}
+
+	_, err = client.SendMessage(ctx, input)
+	if err != nil {
+		return "", fmt.Errorf("failed to enqueue message: %v", err)
+	}
+	return fmt.Sprintf("sucessfully enqueued: %v", event.Word), nil
 }
 
 func main() {
